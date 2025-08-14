f477a56be75db3fceced3cf9a6210217470d6200
shoyub <shoyub.khan01@gmail.com>
2025-08-14 15:25:49 +0530

    secondary commit

--- chat/src/config/cloudinary.ts
+++ chat/src/config/cloudinary.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Api_Key,
+  api_secret: process.env.Api_Secret,
 });
 
 export default cloudinary;
